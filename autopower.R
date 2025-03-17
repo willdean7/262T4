de5b3691d5eb3c78846c39e6aee1d4edfa923702
AlonBoii <alon.robbins@gmail.com>
2025-03-16 19:28:13 -0700

    fixing some things

--- autopower.R
+++ autopower.R
@@ -12,37 +12,21 @@
 
 
 
-auto_power <- function(V, m, A, g = 9.8, p_air=1.2, crolling = 0.015, cdrag = 0.3){
-  V=ifelse((V <0), return("Speed cannot be negative"), V)
-  m= ifelse((m <0), NA, m)
-  A= ifelse((A <0), NA, A)
+# Updated auto_power function with speed conversion
+auto_power <- function(V, m, A, g = 9.8, p_air = 1.2, crolling = 0.015, cdrag = 0.3, kmh_to_ms = TRUE) {
+  # If speed is in km/h, convert it to m/s
+  if (kmh_to_ms) {
+    V <- V * 1000 / 3600  # Convert speed from km/h to m/s
+  }
+  
+  # Ensure that input values for mass and area are valid
+  V <- ifelse((V < 0), return("Speed cannot be negative"), V)
+  m <- ifelse((m < 0), NA, m)
+  A <- ifelse((A < 0), NA, A)
+  
+  # Calculate power in watts (Pb)
   Pb <- (0.5 * p_air * A * cdrag * V^3) + (crolling * m * g * V)
   return(Pb)
 }
 
-auto_power(30, 31752, 16)
-# 217786.3
-auto_power(c(5,10,15), 10000, 20)
-
-kmh_to_ms <- function(kmh){
-  return(kmh * 1000 / 3600)
-}
-
-auto_power(kmh_to_ms(80), 31752, 16)
-# 135328.1
-auto_power(kmh_to_ms(120), 31752, 16)
-# 262251.5
-auto_power(kmh_to_ms(40), 31752, 16)
-# 55812.22
-auto_power(kmh_to_ms(5), 31752, 16)
-# 6490.416
-
-#plot power as a function of speed
-speeds <- seq(0, 120, 1)
-powers <- sapply(speeds, function(x) auto_power(kmh_to_ms(x), 31752, 16))
-plot(speeds, powers, type = "l", xlab = "Speed (km/h)", ylab = "Power (W)", main = "Power as a function of speed")
-
-#recompute for lighter vehicle (15000 kg)
-light_powers <- sapply(speeds, function(x) auto_power(kmh_to_ms(x), 15000, 16))
-lines(speeds, light_powers, col = "red")
 
